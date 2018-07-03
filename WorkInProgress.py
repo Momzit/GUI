5585c9f6f4aaac76fefa4967dee89f54e779e493
Moment Mahlangu <mmahlangu@sansa.org.za>
2018-07-04 00:55:35 +0200

    Pause button works now!!!, on to the next one, reset button.

--- WorkInProgress.py
+++ WorkInProgress.py
@@ -4,52 +4,51 @@ from visual.graph import *
 import wx
 import numpy as np
 
-L = 500
-Hgraph = 50
+running = True
+L = 1000
+Hgraph = 1000
+
+#==========================================================Functions==========================================================
+def pausefunc(evt):
+    global running
+    running = not running
+    if running:
+        pause.SetLabel("Pause")
+    else:
+        pause.SetLabel("Run")
 
 def solarSystem(evt):
     a = Vx.GetValue()
     b = Vy.GetValue()
     c = Vz.GetValue()
     Earth.v = vector(float(a),float(b),float(c))
-    for ii in range(1000):
+    while True:
         rate(100)
 
-        rmag = np.sqrt(Earth.x**2 + Earth.y**2 + Earth.z**2)
-        rhat = (Sun.pos - Earth.pos)/rmag
-        F12 = (10000*rhat)/rmag**2
-        Earth.v += F12
-        Earth.pos += Earth.v
-    print(Earth.v)
+        if running:
+            rmag = np.sqrt(Earth.x**2 + Earth.y**2 + Earth.z**2)
+            rhat = (Sun.pos - Earth.pos)/rmag
+            F12 = (10000*rhat)/rmag**2
+            Earth.v += F12
+            Earth.pos += Earth.v
+            Output.SetValue("The Earth now has this Velocity \n" + str(Earth.v))
 
-def Box(evt):
-    a = Vx.GetValue()
-    b = Vy.GetValue()
-    c = Vz.GetValue()
-    print("("+ a + ","+ b + "," + c + ")")
-
-def choose(evt):
-    selected = evt.GetSelection()
-    if selected == 0: solarSystem(evt)
-    else: Box(evt)
-
-# Create a window. Note that w.win is the wxPython "Frame" (the window).
-# window.dwidth and window.dheight are the extra width and height of the window
-# compared to the display region inside the window. If there is a menu bar,
-# there is an additional height taken up, of amount window.menuheight.
-# The default style is wx.DEFAULT_FRAME_STYLE; the style specified here
-# does not enable resizing, minimizing, or full-sreening of the window.
+#==========================================================Window==========================================================
+#This creates a window, takes in different attributes
 w = window(width=2*(L+window.dwidth), height=L+window.dheight+window.menuheight+Hgraph,
            menus=True, title='Widgets',
            style=wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX)
 
 d = 1
-disp = display(window=w, x=250, y=d, width=800-2*d, height=400-2*d, forward=-vector(0,1,2))
+disp = display(window=w, x=250, y=d, width=1500-2*d, height=850-2*d, forward=-vector(0,1,2))
 gdisplay(window=w, y=50, width=2*(L+window.dwidth), height=Hgraph)
 
+# #==========================================================Sun and Earth==========================================================
+# #The Sun and Earth model
 Sun = sphere(pos=vector(0,0,0), radius=100, color=color.yellow)
 Earth = sphere(pos=vector(200,0,0), radius=10, material=materials.earth, make_trail=True)
 
+#==========================================================Buttons and Widgets==========================================================
 p = w.panel
 font = wx.Font(20,  wx.ROMAN, wx.NORMAL, wx.BOLD)
 l1 = wx.StaticText(p, pos=(105,50), size=(110,50), label='Velocity',
@@ -75,23 +74,20 @@ Vz = wx.TextCtrl(p, pos=(90,200), value='0',
 l4 = wx.StaticText(p, pos=(50,195), label='z')
 l4.SetFont(font)
 
+# font1 = wx.Font(16,  wx.ROMAN, wx.NORMAL, wx.BOLD)
+l5 = wx.StaticText(p, pos=(20,450), label='Velocity Output')
+l5.SetFont(font)
+Output = wx.TextCtrl(p, pos=(10,500), style=wx.TE_MULTILINE,
+            size=(210,100))
 
-mainscreen = wx.Button(p, label='Main Screen', pos=(250,430), size=(110,50))
+mainscreen = wx.Button(p, label='Main Screen', pos=(500,900), size=(110,50))
 #start.Bind(wx.EVT_BUTTON, solarSystem)
 
-pause = wx.Button(p, label='Pause', pos=(580,430), size=(110,50))
-pause.Bind(wx.EVT_BUTTON, Box)
+pause = wx.Button(p, label='Pause', pos=(900,900), size=(110,50))
+pause.Bind(wx.EVT_BUTTON, pausefunc)
 
-reset = wx.Button(p, label='Reset', pos=(900,430), size=(110,50))
-# reset.Bind(wx.EVT_BUTTON, solarSystem)
+reset = wx.Button(p, label='Reset', pos=(1300,900), size=(110,50))
+# reset.Bind(wx.EVT_BUTTON, pausefunc)
 
-start = wx.Button(p, label='Start', pos=(100,300), size=(110,50))
+start = wx.Button(p, label='Input', pos=(100,300), size=(110,50))
 start.Bind(wx.EVT_BUTTON, solarSystem)
-
-#
-# items = []
-# items.append('Earth-sun')
-# items.append('Box')
-# pulldown_menu = wx.Choice(p, choices=items, pos=(L+250,550))
-# pulldown_menu.Bind(wx.EVT_CHOICE, choose)
-# pulldown_menu.SetSelection(0)
